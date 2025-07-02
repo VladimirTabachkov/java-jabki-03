ed6a990801dc643c2e89f154793add7ef3e4f09f
seriy <Vladimir.Tabachkov@smartds.ru>
2025-07-02 15:33:55 +0300

    Мое решение

--- src/MainTest.java
+++ src/MainTest.java
@@ -8,22 +8,22 @@ class MainTest {
 
     @Test
     public void sumRange() {
-        Assertions.assertEquals(10, Main.sumRange(1,4));
+        Assertions.assertEquals(10, Main.sumRange(1, 4));
     }
 
     @Test
     void addn() {
-        Assertions.assertEquals(10, Main.add(4,6));
+        Assertions.assertEquals(10, Main.add(4, 6));
     }
 
     @Test
     void adds() {
-        Assertions.assertEquals("Пример", Main.add("При","мер"));
+        Assertions.assertEquals("Пример", Main.add("При", "мер"));
     }
 
     @Test
     void findMax() {
-        int[] nums = { -100, 4, 5, 9 };
+        int[] nums = {-100, 4, 5, 9};
         Assertions.assertEquals(9, Main.findMax(nums));
     }
 
@@ -34,23 +34,23 @@ class MainTest {
 
     @Test
     void calculateAreaC() {
-        Assertions.assertEquals(78.54, (double)(Math.round(Main.calculateArea(5)*100))/100.0);
+        Assertions.assertEquals(78.54, (double) (round(Main.calculateArea(5) * 100)) / 100.0);
     }
 
     @Test
     void CalculateAreaR() {
-        Assertions.assertEquals(30, (double)(Math.round(Main.calculateArea(5, 6 )*100))/100.0);
+        Assertions.assertEquals(30, (double) (round(Main.calculateArea(5, 6) * 100)) / 100.0);
     }
 
     @Test
     void averageNums() {
         double[] dnums = {1, 2, 3};
-        Assertions.assertEquals(2, (double)(Math.round(Main.AverageNums(dnums)*100))/100.0);
+        Assertions.assertEquals(2, (double) (round(Main.AverageNums(dnums) * 100)) / 100.0);
     }
 
     @Test
     void pifogor() {
-        Assertions.assertEquals(1.41, (double)(Math.round(Main.Pifogor(1,1)*100))/100.0);
+        Assertions.assertEquals(1.41, (double) (round(Main.Pifogor(1, 1) * 100)) / 100.0);
 
     }
 }
